a48afa540dd40f878243438bd9766022ea3ff2fa
Kenji Nakamura <dragondev1017@gmail.com>
2022-02-09 05:05:33 +0900

    Parse xml and store

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module IRSApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths << Rails.root.join('source')
   end
 end
