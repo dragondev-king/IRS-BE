c7489500a0c74d602b280ee35f7217c41cfcefd8
Kenji Nakamura <dragondev1017@gmail.com>
2022-02-11 22:26:26 +0900

    Update filings controller

--- app/controllers/filings_controller.rb
+++ app/controllers/filings_controller.rb
@@ -3,9 +3,9 @@ class FilingsController < ApplicationController
     #GET /filings?filer=xxx
     def index
 
-        @filings = Filing.all
+        @filings = Filing.select('filings.id, filer_id, filers.name as filer_name, recipient_id, recipients.name as recipient_name, awards.purpose as purpose, awards.tax_period as tax_period, awards.amount as amount').joins(:recipient, :filer, :awards)
         if params[:filer]
-            @filings = Filing.where(filer_id: params[:filer])
+            @filings = @filings.where(filer_id: params[:filer])
         end
         render json: @filings
     end
